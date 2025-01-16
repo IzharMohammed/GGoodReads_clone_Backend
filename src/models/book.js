533da0b40bc48245e46c52df44c65ea5794b7b23
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-01-16 23:42:35 +0530

    Initial commit

---
+++ src/models/book.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "can't be blank"],
+    },
+    description: {
+        type: String,
+        required: [true, "can't be blank"],
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+      // type: String,
+        ref: 'Author'
+    },
+    genres: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+         //   type: String,
+            ref: 'Genre' 
+        }
+    ],
+    pages: {
+        type: Number,
+        required: true
+    },
+    publishDate: {
+        type: String
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        required: [true, "Can't be blank"]
+    }
+}, {timestamps: true});
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
