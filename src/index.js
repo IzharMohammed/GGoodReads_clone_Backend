abf13006e3ffc39b273060ea7d9c4179e2b32c27
MohammedIzhar77 <izharmohammed21@gmail.com>
2025-01-17 01:54:43 +0530

    Testing production

--- src/index.js
+++ src/index.js
@@ -10,19 +10,21 @@ const logger = require('./config/logger');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
 
 app.get('/home', (req, res) => {
     res.send('<h1>Home</h1>');
-})
+});
 
-const setupAndStartServer = function() {
-    app.listen(PORT, async function() {
-        console.log(`Server started at PORT ${PORT}`);
-        await connect();
-        console.log('Mongo db connected');
-    });
-}
+const startServer = async function() {
+    console.log(`Server started at PORT ${PORT}`);
+    await connect();
+    console.log('Mongo db connected');
+};
 
-setupAndStartServer();
+// Export the app for Vercel compatibility
+module.exports = async (req, res) => {
+    await startServer(); // Ensure the database is connected
+    return app(req, res); // Delegate requests to Express
+};
